3626d89799c0299dc44f02f1ba9436d28134a8df
GeorgeH <georgeharland12@gmail.com>
2023-11-28 22:54:09 +0000

    image styles

--- components/homeTitle/styled.ts
+++ components/homeTitle/styled.ts
@@ -7,7 +7,7 @@ type WrapperProps = {
 
 export const Wrapper = styled.div<WrapperProps>`
   display: flex;
-  font-size: ${({ screenSize }) => screenSize > breakpoints.bigtablet ? '28px' : '24px'};
+  font-size: ${({ screenSize }) => screenSize > breakpoints.bigtablet ? '26px' : '22px'};
   padding: ${({ screenSize }) => screenSize > breakpoints.bigtablet ? '2rem 3rem' : '1rem 0'};
   margin-left: ${({ screenSize }) => screenSize > breakpoints.bigtablet ? '7.5%' : '3%'};
   margin-bottom: 1rem;
--- components/profileImage/styled.ts
+++ components/profileImage/styled.ts
@@ -3,9 +3,11 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   position: absolute;
-  top: -1rem;
+  top: 3rem;
+  right: 10rem;
 `
 
 export const ProfileImage = styled.img`
   border-radius: 100%;
+  max-width: 10rem;
 `
